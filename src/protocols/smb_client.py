b4dd17bf056aca77971c5a4d3c28a71e80a01bdc
Caio <caiow.wk@gmail.com>
2022-06-19 19:02:01 -0300

    feat: add delete method method

--- src/protocols/smb_client.py
+++ src/protocols/smb_client.py
@@ -13,23 +13,32 @@ class SmbClient:
 
     def persist(self, file_path: str) -> None:
         try:
+            self.__get_connection()
             with open(file_path, 'rb') as f:
                 file_path_to_persist = os.path.join(self.__config.smb_remote_folder_to_persist,
                                                     os.path.basename(file_path))
-                self.__get_connection().storeFile(self.__config.smb_remote_folder_share_to_watch,
-                                                  file_path_to_persist, f)
+                self.__operations.storeFile(self.__config.smb_remote_folder_share_to_watch,
+                                            file_path_to_persist, f)
         except Exception as ex:
             logging.error('Fail to persist: ', ex)
         finally:
             self.__close_connection()
 
     def delete_recursively(self, file_path: str):
-        # self.__operations.deleteFiles(self.__shared_folder, "test/*")
-        pass
+        try:
+            self.__get_connection()
+            file_path_to_delete = os.path.join(self.__config.smb_remote_folder_to_persist,
+                                               os.path.basename(file_path))
+            self.__operations.deleteFiles(self.__config.smb_remote_folder_share_to_watch, file_path_to_delete)
+        except Exception as ex:
+            logging.error('Fail to persist: ', ex)
+        finally:
+            self.__close_connection()
 
     def __get_connection(self):
         self.__smb_client.connect()
-        return self.__smb_client.operations
+        self.__operations = self.__smb_client.operations
+        return self.__operations
 
     def __close_connection(self):
         self.__smb_client.close()
