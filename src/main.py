51e6898a403833ed76c1ebef36fabe1d80b18630
Caio <caiow.wk@gmail.com>
2022-06-19 04:43:00 -0300

    chore: remove idea from git:

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 import time
 from dotenv import load_dotenv
+from src.config.logging import LoggingHandler
 
 if __name__ == "__main__":
     load_dotenv()
 
-    from src.config.logging import LoggingHandler
     from src.watchers.main import Observer
 
     LoggingHandler()
--- watch_and_send.sh
+++ watch_and_send.sh
@@ -1,2 +1,5 @@
+# cat ".env.local.$PROFILE" > .env.back
+# cat .env | xargs printf -- 'export %s\n'
+
 export PYTHONPATH=$(pwd)
 python3 src/main.py
